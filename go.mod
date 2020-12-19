51d07eb15cba69339541697decfeba06ccb03dd8
snimmagadda1 <saiguy@me.com>
2020-12-19 00:38:22 -0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/snimmagadda1/graphql-api
+
+go 1.13
+
+require (
+	github.com/99designs/gqlgen v0.13.0
+	github.com/agnivade/levenshtein v1.1.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/matryer/moq v0.1.4 // indirect
+	github.com/mitchellh/mapstructure v1.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+	github.com/vektah/dataloaden v0.3.0 // indirect
+	github.com/vektah/gqlparser/v2 v2.1.0
+	golang.org/x/mod v0.4.0 // indirect
+	golang.org/x/tools v0.0.0-20201218024724-ae774e9781d2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ graph/schema.resolvers.go
@@ -0,0 +1,29 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/snimmagadda1/graphql-api/graph/generated"
+	"github.com/snimmagadda1/graphql-api/graph/model"
+)
+
+func (r *queryResolver) Users(ctx context.Context) ([]*model.User, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+type mutationResolver struct{ *Resolver }

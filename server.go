7d90bdad650db0a2ce1486e7232e940b6f465f71
snimmagadda1 <saiguy@me.com>
2020-12-19 01:45:56 -0600

    Cnx

--- server.go
+++ server.go
@@ -7,13 +7,18 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/joho/godotenv"
 	"github.com/snimmagadda1/graphql-api/graph"
+	"github.com/snimmagadda1/graphql-api/graph/database"
 	"github.com/snimmagadda1/graphql-api/graph/generated"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	godotenv.Load()
+	database.InitDB()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort

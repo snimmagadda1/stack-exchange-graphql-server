3427d940ead8a19f8fadf6b1eab97d130463d758
snimmagadda1 <saiguy@me.com>
2020-12-20 19:34:03 -0600

    Clean up

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -135,11 +135,3 @@ func (r *queryResolver) AllPostsCursor(ctx context.Context, first *int, after *s
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-type mutationResolver struct{ *Resolver }

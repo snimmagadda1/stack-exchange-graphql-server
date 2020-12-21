f9a96d991b7114d98442ab9f36e92d1890d310a1
snimmagadda1 <saiguy@me.com>
2020-12-21 11:45:53 -0600

    saving

--- dev/stage.sh
+++ dev/stage.sh
@@ -2,4 +2,3 @@
 echo "WARNING: downloading and unzipping data for: $1";
 curl -O "https://archive.org/download/stackexchange/${1}.stackexchange.come.7z"
 mkdir "${1}.stackexchange.come.7z/"
-unzip -d "${1}.stackexchange.come.7z/" "${1}.stackexchange.come.7z" 
